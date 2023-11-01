a5514ecd2603e980e2314ac5ad1265428e3ad1be
Astha956 <jainash778@gmail.com>
2023-11-01 23:15:50 +0530

    Initial commit

---
+++ src/data.jsx
@@ -0,0 +1,117 @@
+import Agra from './assets/Agra.jpeg';
+import Jaipur from './assets/Jaipur.jpeg';
+import Darjeeling from './assets/Darjeeling.jpeg';
+import Jaisalmer from './assets/Jaisalmer.jpeg';
+import Varanasi from './assets/Varanasi.jpeg';
+import Goa from './assets/Goa.jpeg';
+import Kochi from './assets/Kochi.jpeg';
+import OIP from './assets/OIP.jpeg';
+import shimla from './assets/shimla.jpeg';
+import amirt from './assets/amirt.jpeg';
+import otty from './assets/otty.jpeg';
+import aurga from './assets/aurga.jpeg';
+import udaipur from './assets/udaipur.jpeg';
+import chennai from './assets/chennai.jpeg';
+
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: Agra, 
+		price: "35,758",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: Jaipur,
+		price: "82,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: Goa,
+		price: "29,695",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: Varanasi,
+		price: "45,889",
+	},
+	{
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: Darjeeling,
+		price: "78,595",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: Jaisalmer,
+		price: "68,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: Kochi,
+		price: "68,595",
+	},
+	{
+		id: 8,
+		name: "Mumbai",
+		info: "Mumbai, formerly known as Bombay, is the financial and entertainment capital of India. The city is famous for its Bollywood film industry, historic landmarks like the Gateway of India, and its bustling street markets.",
+		image: OIP,
+		price: "45,820",
+	  },
+	  {
+		id: 9,
+		name: "Udaipur",
+		info: "Udaipur, often referred to as the 'City of Lakes,' is known for its picturesque lakes, palaces, and vibrant art scene. It's a popular tourist destination for its romantic ambiance and stunning architecture.",
+		image: udaipur,
+		price: "54,320",
+	  },
+	  {
+		id: 10,
+		name: "Shimla",
+		info: "Shimla is a beautiful hill station in Himachal Pradesh, India. It's known for its colonial-era architecture, scenic landscapes, and pleasant climate. Shimla offers a peaceful escape from the hustle and bustle of city life.",
+		image: shimla,
+		price: "35,945",
+	  },
+	  {
+		id: 11,
+		name: "Chennai",
+		info: "Chennai, the capital of Tamil Nadu, is a vibrant coastal city known for its cultural heritage, temples, and sandy beaches. It's a hub of South Indian cuisine and traditional arts.",
+		image: chennai,
+		price: "41,320",
+	  },
+	  {
+		id: 12,
+		name: "Amritsar",
+		info: "Amritsar is a city in the state of Punjab, famous for the Golden Temple, a significant religious and cultural landmark for Sikhs. The city is also known for its rich cuisine and warm hospitality.",
+		image: amirt,
+		price: "38,695",
+	  },
+	  {
+		id: 13,
+		name: "Ooty",
+		info: "Ooty, also known as Udhagamandalam, is a popular hill station in Tamil Nadu, India. It's known for its lush green landscapes, tea gardens, and a cool climate. Ooty is a perfect destination for nature lovers.",
+		image: otty,
+		price: "33,750",
+	  },
+	  {
+		id: 14,
+		name: "Aurangabad",
+		info: "Aurangabad is a city in Maharashtra, India, known for its historical and architectural heritage. It's home to the UNESCO-listed Ajanta and Ellora Caves, stunning rock-cut cave complexes featuring ancient Buddhist, Hindu, and Jain temples.",
+		image: aurga,
+		price: "29,995",
+	  },
+];
+
+export default data;
